97ff02870cf7d8d26826c645a93cd2af70086eb8
K.Kimura <dotnsf@gmail.com>
2016-01-16 16:54:16 +0900

    add credential code for Bluemix

--- credentials.php
+++ credentials.php
@@ -1,5 +1,15 @@
 <?php
 # http://www.alchemyapi.com/api/register.html
 $apikey = '(Your AlchemyAPI Key)';
+
+// Bluemix 環境であれば上記の設定は不要
+if( getenv( 'VCAP_SERVICES' ) ){
+  $vcap = json_decode( getenv( 'VCAP_SERVICES' ), true );
+  
+  $credentials1 = $vcap['alchemy_api'][0]['credentials'];
+  if( $credentials1 != NULL ){
+    $apikey = $credentials1['apikey'];
+  }
+}
 ?>
 
